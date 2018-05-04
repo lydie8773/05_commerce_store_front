80683aea7b38f923189a1e38a5a0755335856d90
Yuanqin DENG <yuanqin.deng@decathlon.com>
2018-05-04 15:17:39 +0200

    6

--- db/db.sql
+++ db/db.sql
@@ -31,3 +31,6 @@ CREATE TABLE categories_products (
     categories_id VARCHAR(255),
     products_id VARCHAR(255)
 );
+
+
+DATABASE_URL: postgres://exnghhjvpddbbd:fddb3216a293a5e692688166e274a792a05919b1753e401a66a94a688ee17288@ec2-54-204-39-46.compute-1.amazonaws.com:5432/de4ntc9btm331o
